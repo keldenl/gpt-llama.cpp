6b2934d33fcaf9b7900d58b1757f9aaeac4527e5
keldenl <keldenlin@gmail.com>
2023-06-09 22:27:38 -0700

    add wizardlm template style

---
+++ ChatEngine/WizardLM.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from './index.js';
+
+/**
+ * This works for the following models (but is not limited to):
+ * WizardLM
+ */
+export class WizardLM extends ChatEngine {
+	constructor() {
+		super({
+			roleMap: { assistant: 'ASSISTANT', user: 'USER', system: 'SYSTEM' },
+			instructions:
+				"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.",
+		});
+		this.stopPrompts = [
+			...this.stopPrompts,
+			'</s>',
+			'\n</s>',
+			'\n</s',
+			'[end of text]',
+		];
+	}
+}
--- ChatEngine/initializeChatEngine.js
+++ ChatEngine/initializeChatEngine.js
@@ -1,4 +1,5 @@
 import { ChatEngine } from './index.js';
+import { WizardLM } from './WizardLM.js';
 import { AlpacaEngine } from './AlpacaEngine.js';
 import { Vicuna1_1Engine } from './Vicuna1_1Engine.js';
 import { VicunaEngine } from './VicunaEngine.js';
@@ -7,21 +8,24 @@ import { DanteEngine } from './Dante.js';
 
 export function initializeChatEngine(modelPath) {
 	const engineMap = {
+		wizardlm: WizardLM,
 		'vicuna1.1': Vicuna1_1Engine,
 		vicuna: VicunaEngine,
 		alpaca: AlpacaEngine,
 		'nous-hermes': AlpacaEngine,
-    	redpajama: RedPajamaEngine,
-    	dante: DanteEngine,
+		redpajama: RedPajamaEngine,
+		dante: DanteEngine,
 		default: ChatEngine,
 	};
 
 	for (const keyword of Object.keys(engineMap)) {
 		if (modelPath.toLowerCase().includes(keyword.toLowerCase())) {
-      console.log(`> ${keyword.toUpperCase()} MODEL DETECTED. LOADING ${keyword.toUpperCase()} ENGINE...`)
+			console.log(
+				`> ${keyword.toUpperCase()} MODEL DETECTED. LOADING ${keyword.toUpperCase()} ENGINE...`
+			);
 			return new engineMap[keyword.toLowerCase()]();
 		}
 	}
-  console.log(`> AUTO MODEL DETECTION FAILED. LOADING DEFAULT CHATENGINE...`)
+	console.log(`> AUTO MODEL DETECTION FAILED. LOADING DEFAULT CHATENGINE...`);
 	return new engineMap['default']();
 }
